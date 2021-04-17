02cc99d4ac3400d82a5979e95c68f1cdcd78c5cc
Paulo Gonçalves <paulogoncalves.m@gmail.com>
2021-04-17 11:58:46 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
+#include <Rect.h>
 
 int main( int argc, char ** argv ) {
-    
+    std::cout << "Hello World" << std::endl;   
+    Rect r1;
+    Point delta (0,0); 
     return 0;
 }
